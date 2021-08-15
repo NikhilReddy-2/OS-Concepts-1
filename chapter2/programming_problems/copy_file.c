5f1a1dea0ebc3609c3633d189a6716258536de44
NikhilReddy-2 <nikhil@kawa.space>
2021-08-15 23:09:06 +0530

    Initial commit, added usage check

--- chapter2/programming_problems/copy_file.c
+++ chapter2/programming_problems/copy_file.c
@@ -1,23 +1,31 @@
+/*
+This file contains the copy program problem from the book OS-Concepts.
+
+./copy <file1 Path> <file2 Path> [-option1] [-option2]
+
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<fcntl.h>
+
 
 static void usage();
 
 int main(int argc, char * argv[])
 {
-    if( argc < 2){
+    if(argc < 3){
         usage();
     }
-    
+
+
+    return 0;
 }
 
-/*
-brief: Error message to display correct usage
-*/
-static void usage(){
-    fprintf(stderr,"usage: ./copy <file1> <file2> \n");
+static void usage()
+{
+    fprintf(stderr,"usage: ./copy <FILE1> <FILE2> [-options]");
     exit(-1);
 }
+
+
+
