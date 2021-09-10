bfa0723c0e51189bbc2bbf2ba5a8996fdeada42e
NikhilReddy-2 <nikhil@kawa.space>
2021-09-10 16:35:36 +0530

    Map should be populated with 0s to track PIDs

--- chapter3/3.13/pid_identify.c
+++ chapter3/3.13/pid_identify.c
@@ -4,6 +4,24 @@
 #define MIN_PID 300
 #define MAX_PID 5000
 
-int allocate_map(void);     //Creates and inits a data structure for representing PIDs, returns 1-failure 0- success
+//Creates and inits a data structure for representing PIDs, returns 1-failure 0- success
+int allocate_map(int * pid_map){
+    for (int i=0;i<(MAX_PID-MIN_PID);i++){
+        pid_map[i] = 0;
+    }
+    return 0;
+}
 int allocate_pid(void);     //Allocates and returns a PID, returns 1-failure 0-success
 void release_pid(int pid);  //Releases a PID
+
+
+
+int main(){
+    int pid_map[MAX_PID-MIN_PID];
+
+    if((allocate_map(pid_map)) == 0){
+        printf("Map populated with 0s\n");
+    }
+
+    return 0;
+}
